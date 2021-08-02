dcb601456c028e8ae57436c16459a8bd6623a34f
alec <kaltschmidalec@gmail.com>
2021-08-02 09:22:17 -0700

    creating dropdown bar

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const templates = "http://localhost:3000/templates"
+const playButton = el('play-button')
+const pageBody = el('main-content')
 
+function el(id){
+    return document.getElementById(id)
+}
+
+fetch(templates)
+.then(resp => resp.json())
+.then(data => console.log(data))
+
+
+playButton.addEventListener('click', () => {
+    showDropdown()
+})
+
+function showDropdown() {
+    const label = document.createElement('label')
+    label.innerText = 'Choose Your Template'
+    const dropDown = document.createElement('select')
+    pageBody.append(label, dropDown)
+}
